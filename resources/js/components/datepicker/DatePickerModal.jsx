fec46564e60edc53bb1dd48fc102049899002030
devwork-trifonov <devwork.trifonov@gmail.com>
2020-09-28 11:16:06 +1000

    change the location of import styles for react-datepicker

--- resources/js/components/datepicker/DatePickerModal.jsx
+++ resources/js/components/datepicker/DatePickerModal.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import DatePicker from "react-datepicker";
 import { registerLocale } from "react-datepicker";
 import ru from "date-fns/locale/ru";
-import "react-datepicker/dist/react-datepicker.css";
 registerLocale("ru", ru);
 
 export function DatePickerModal({
