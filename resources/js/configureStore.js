ca282c325f36b50644d6af96a0af535b6307d4de
devwork-trifonov <devwork.trifonov@gmail.com>
2021-10-18 17:52:24 +1000

    preparation for deployment on heroku

--- resources/js/configureStore.js
+++ resources/js/configureStore.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware, compose } from "redux"
 import thunkMiddleware from "redux-thunk"
 import rootReducer from "./reducers/rootReducer"
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = compose
 export default function configureStore() {
   return createStore(
     rootReducer,
